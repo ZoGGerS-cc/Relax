0f8ce9d7fd4098da47abae9c279f2c6143c19f0a
ZoGGerS-cc <73020054+ZoGGerS-cc@users.noreply.github.com>
2024-02-04 12:48:54 +0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+//Mod
+var mod = Vars.mods.locateMod("relax");
+mod.meta.displayName = "[#BA4A00]Relax";
+mod.meta.description = "[#BA4A00]Мод на стадии разработки!";
+mod.meta.author = "[#BA4A00]ZoGGerS-Dim4ik";
+
+//Block
+require("blocks/drill");
+
+//Print_Name
+print("[green]☆relax_alpha☆");
+print("[green]☆by ZoGGerS☆");
